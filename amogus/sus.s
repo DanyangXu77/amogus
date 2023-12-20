b8932de77cd11bdd9e3d473184b2b7b11d9cd1f8
DanyangXu77 <116617888+DanyangXu77@users.noreply.github.com>
2023-12-20 19:36:04 +0000

    test

---
+++ amogus/sus.s
@@ -0,0 +1,22 @@
+section	.text
+	global _start
+
+section	.data
+txt db 'done', 0xa
+len equ $ - txt
+
+_start:
+	mov ecx, 1000000000
+
+l:
+	loop l
+	mov	eax, 4
+	mov ebx, 1
+	mov	edx, len
+	mov	ecx, txt
+	int 0x80
+
+	int 0x80
+	mov	eax, 1
+	mov ebx, 0
+	int	0x80
